ddc31de97918d6f732090fadfb034acd9c8f2364
Thanuj <thanujdevil@gmail.com>
2024-05-25 16:57:20 +0530

    counterup added

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React,{useEffect} from "react";
 import aboutimage from "../assets/about.jpg";
-import CountUp from "react-countup";
+import CountUp, { useCountUp } from "react-countup"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function AboutUs() {
+
+
+  useCountUp({
+    ref: 'counter',
+    end: 1234567,
+    enableScrollSpy: true,
+    scrollSpyDelay: 1000,
+  });
+
+
+  useEffect(()=>{
+    AOS.init();
+
+  },[]);
+  
+
+
   return (
-    <div className="container mx-auto px-5 flex justify-center gap-5 mt-20">
+    <div  data-aos="fade-left"  className="container mx-auto px-5 flex justify-center gap-5 mt-20">
       <div className="flex flex-col gap-5 w-[492px] ">
         <h2 className="text-4xl font-bold">About Us</h2>
         <p className="text-gray-500">
@@ -21,15 +40,15 @@ function AboutUs() {
         </p>
         <div className="flex gap-4">
           <div className="bg-creamy rounded-2xl p-3 flex flex-col justify-center items-center w-[150px]">
-            <h3 className="text-3xl font-bold">{<CountUp end={2011} />}</h3>
+            <h3 className="text-3xl font-bold">{<CountUp separator='' end={2011}  enableScrollSpy/>}</h3>
             <p>Year of est.</p>
           </div>
           <div className="bg-creamy rounded-2xl p-3 flex flex-col justify-center items-center w-[150px]">
-            <h3 className="text-3xl font-bold">{<CountUp end={880} />}</h3>
+            <h3 className="text-3xl font-bold">{<CountUp end={880}  enableScrollSpy />}</h3>
             <p>No of products</p>
           </div>
           <div className="bg-creamy rounded-2xl p-3 flex flex-col justify-center items-center w-[150px]">
-            <h3 className="text-3xl font-bold">{<CountUp end={1200} />}</h3>
+            <h3 className="text-3xl font-bold">{<CountUp end={1200}  enableScrollSpy/>}</h3>
             <p>Employee count</p>
           </div>
         </div>
